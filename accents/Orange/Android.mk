0968947b452ca580f13c1ca86ff83c7c0bc599ff
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-05-18 19:33:56 +0000

    Organize strings
    Change-Id: I429737b1b0a62074e6aa30e4369daae965f07a5c

--- accents/Orange/Android.mk
+++ accents/Orange/Android.mk
@@ -8,4 +8,10 @@ LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    vendor/themes/common/res
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay
+
 include $(BUILD_PACKAGE)
