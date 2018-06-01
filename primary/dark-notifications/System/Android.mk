491703466f058375383d83a0a306c1b85bb9804a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-06-01 20:30:45 -0300

    Notification theme switch [2/3]
    Change-Id: Ib9bd483f5c7bbe9abc7bf7320ba4fc0444a459c6

---
+++ primary/dark-notifications/System/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := SystemDarkNotifications
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    vendor/themes/primary/dark/System/res \
+    vendor/themes/common/res
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay
+
+include $(BUILD_PACKAGE)
