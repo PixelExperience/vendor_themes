58cb212b3c1a52ce59a82cfd2499026da1fe4a96
Alex Cruz <alex@dirtyunicorns.com>
2018-09-20 04:50:10 -0500

    Set themes to compile
    Change-Id: Iac4e392ecc7625c04eda89ecd3b2fc98b41ce496

--- common.mk
+++ common.mk
@@ -3,3 +3,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 
 # Sounds
 include vendor/themes/sounds.mk
+
+# Accents
+include vendor/themes/accents.mk
+
+# Themes
+include vendor/themes/themes.mk
